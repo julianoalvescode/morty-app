27030d76436ccff4f4d4b8a680432b33698d0a76
Juliano Alves <julianoalves@me>
2023-09-27 23:13:09 -0300

    feat: 🎸 add lazy loading in images

--- src/design-system/components/Card/index.tsx
+++ src/design-system/components/Card/index.tsx
@@ -15,6 +15,7 @@ export function Card(params: CardProps) {
         className={styles["card-image"]}
         src={params?.image}
         alt={params?.name}
+        loading="lazy"
       />
       <div className={styles["card-info-insider"]}>
         <div className={styles["card-info-text-container"]}>
